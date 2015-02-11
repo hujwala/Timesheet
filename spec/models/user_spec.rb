e2d8904741c7712659e27290d6ccaf6d443e2d2d
hujwala <sjayaramu@qwinixtech.com>
2015-02-11 13:28:13 +0530

    updated rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,35 +11,18 @@ RSpec.describe User,:type => :model do
     
     describe User do
       it{should validate_presence_of :name}
-      it { should allow_value('Ujwala').for(:name )}
+      it { should allow_value('ujwala').for(:name )}
       it{should validate_presence_of :email}
       it { should allow_value('ujwala@qwinixtech.com').for(:email )}
       it { should validate_presence_of :password }
-      it { should allow_value('Password@1').for(:password )}
+     
     end
   end
 
-  it "should validate name lenght" do
-
-    user.name = "pr"*50
-    user.valid?
-    expect(user.errors[:name].size).to be 1
-    expect(user).to be_invalid
-
-    user.name = "pr"
-    user.valid?
-    expect(user.errors[:name].size).to be 1
-    expect(user).to be_invalid
-
-    user.name = "Ujwala"
-    user.valid?
-    expect(user.errors[:name].size).to be 0
-    expect(user).to be_valid
-
-  end
+  
   it "should validate password lenght" do
 
-    user.password = "ujwala"
+    user.password = "ujwal"
     user.valid?
     expect(user.errors[:password].size).to be 1
     expect(user).to be_invalid
@@ -50,21 +33,7 @@ RSpec.describe User,:type => :model do
     expect(user).to be_valid
   end
 
-  it "should validate name" do
-    
-    ["Ujwala", "Patel"].each do |n|
-      user.name = n
-      value = user.valid?
-      expect(value).to be_truthy
-    end
-
-    
-    ["p", "m"].each do |n|
-      user.name = n
-      value = user.valid?
-      expect(value).to be_falsy
-    end
-  end
+  
 
   it "should validate email" do
     
@@ -91,7 +60,7 @@ RSpec.describe User,:type => :model do
     end
 
     
-    ["Password", "Pa"].each do |n|
+    ["Pass", "Pa"].each do |n|
       user.password = n
       value = user.valid?
       expect(value).to be_falsy
