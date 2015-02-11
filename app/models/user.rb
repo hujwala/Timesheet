54cd0239d84671131d051c5d1a8fd2f6389060e3
hujwala <sjayaramu@qwinixtech.com>
2015-02-11 12:19:38 +0530

    Changes to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ActiveRecord::Base
     has_secure_password
 
-	validates :name, :presence =>true,
-	:format => {:with => ConfigCenter::GeneralValidations::NAME_FORMAT_REG_EXP}
+	validates :name, :presence =>true
 
 	validates :email, :presence =>true,
-	:uniqueness => true,
-	:format => {:with =>ConfigCenter::GeneralValidations::EMAIL_FORMAT_REG_EXP }
+	:uniqueness => true
 
 	validates :password, :presence =>true,
 	:length => {:minimum => 6 }
