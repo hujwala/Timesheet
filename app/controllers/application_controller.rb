ab63a629ce5625ae28b4065e0f52b0876d6a766a
hujwala <sjayaramu@qwinixtech.com>
2015-02-18 10:46:45 +0530

    added callback function for prevent button

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  before_filter :set_cache_buster  
   helper_method :current_user
 
   
@@ -8,6 +8,11 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
   end 
+
+  def set_cache_buster
+    response.headers["Cache-Control"] = "no-cache, no-store, max-age=0, must-revalidate"
+    response.headers["Pragma"] = "no-cache"
+  end
 
   def require_login
     unless session[:user_id]
